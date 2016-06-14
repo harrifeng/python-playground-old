eeea5d1b743dfbed5827dbd34cf060870547d4dc
Haoran Feng <haoran.feng@sap.com>
2016-06-13 23:48:14 -0700

    9th

---
+++ hardway/9.py
@@ -0,0 +1,15 @@
+# Here's some new strnge stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+print "Here are the months in one line: %r" % months
+
+print """
+There's somthing going on here.
+With the threee double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
