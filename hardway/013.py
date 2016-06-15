f1d3f055a981d84024ae483d16ea1099ae9a3e47
Haoran Feng <haoran.feng@sap.com>
2016-06-14 23:14:47 -0700

    new input

---
+++ hardway/013.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+##################################################
+# <===================OUTPUT===================> #
+# $ python 013.py first second third             #
+# The script is called: 013.py                   #
+# Your first variable is: first                  #
+# Your second variable is: second                #
+# Your third variable is: third                  #
+##################################################
